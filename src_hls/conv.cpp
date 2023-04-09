41cc922dc7b9c41cfd24da3fbe584c3e7bd3dca0
Vinam Arora <vinam@posteo.de>
2023-04-08 20:22:38 -0400

    Adds padding to conv.cpp and cleans up the code

--- src_hls/conv.cpp
+++ src_hls/conv.cpp
@@ -1,30 +1,52 @@
 #include "util.h"
 
-template <int IN_BUF_DEPTH, int IN_BUF_HEIGHT, int IN_BUF_WIDTH,
-         int OUT_BUF_DEPTH, int OUT_BUF_HEIGHT, int OUT_BUF_WIDTH,
-    int KERNEL_HEIGHT, int KERNEL_WIDTH, int STRIDE>
+#define CONV_DIM(IDIM, KDIM, PD, ST) ((IDIM - KDIM + 2*PD)/ST + 1)
+// Compute the output dimension given the input dimension, kernel dimension,
+// stride and padding
+
+/* Convolution kernel template
+ *
+ * Template parameters:
+ *   ID: Input depth
+ *   IH: Input height
+ *   IW: Input width
+ *   KD: Kernel depth
+ *   KH: Kernel height
+ *   KW: Kernel width
+ *   ST: Stride
+ *   PD: Padding
+ * Templated with the following assumptions:
+ * - Padding and Stride are same on all sides
+ *
+ */
+template <int ID, int IH, int IW, int KD, int KH, int KW, int ST, int PD>
 void conv (
-        fm_t Y_buf[OUT_BUF_DEPTH][OUT_BUF_HEIGHT][OUT_BUF_WIDTH],
-        fm_t X_buf[IN_BUF_DEPTH][IN_BUF_HEIGHT][IN_BUF_WIDTH],
-        wt_t W_buf[OUT_BUF_DEPTH][IN_BUF_DEPTH][KERNEL_HEIGHT][KERNEL_WIDTH],
-        wt_t B_buf[OUT_BUF_DEPTH]
+        fm_t Y_buf[KD][CONV_DIM(IH, KH, ST, PD)][CONV_DIM(IW, KW, ST, PD)],
+        fm_t X_buf[ID][IH][IW],
+        wt_t W_buf[KD][ID][KH][KW],
+        wt_t B_buf[KD]
         )
 {
 
-    const int S = STRIDE;
+    const int OD = KD;
+    const int OH = CONV_DIM(IH, KH, ST, PD);
+    const int OW = CONV_DIM(IW, KW, ST, PD);
 
-    for (int of = 0; of < OUT_BUF_DEPTH; of++)
-        for (int oh = 0; oh < OUT_BUF_HEIGHT; oh++)
-            for (int ow = 0; ow < OUT_BUF_WIDTH; ow++)
-                for (int id = 0; id < IN_BUF_DEPTH; id++)
-                    for (int kh = 0; kh < KERNEL_HEIGHT; kh++)
-                        for (int kw = 0; kw < KERNEL_WIDTH; kw++)
+    for (int of = 0; of < KD; of++)
+        for (int oh = 0; oh < OH; oh++)
+            for (int ow = 0; ow < OW; ow++)
+                for (int id = 0; id < ID; id++)
+                    for (int kh = 0; kh < KH; kh++)
+                        for (int kw = 0; kw < KW; kw++)
                         {
                             if (id == 0 && kh == 0 && kw == 0)
                                 Y_buf[of][oh][ow] = B_buf[of];
 
-                            int i = S*oh + kh;
-                            int j = S*ow + kw;
+                            int i = (ST * oh) - PD + kh;
+                            int j = (ST * ow) - PD + kw;
+
+                            if (i < 0 || j < 0 || j >= IH || j >= IW)
+                                continue;
 
                             Y_buf[of][oh][ow] += X_buf[id][i][j] * W_buf[of][id][kh][kw];
                         }
