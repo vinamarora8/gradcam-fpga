25fcd449679683b8fd80e30b0f9589f6f3c71e68
Vinam Arora <vinam@posteo.de>
2023-04-19 23:46:40 -0400

    Removes checking of *_c1_out outputs, and adds PASS/FAIL

--- src_py/check_mse.py
+++ src_py/check_mse.py
@@ -9,29 +9,30 @@ file_list = [
     "input.bin",
     "conv1_out.bin",
     "maxpool_out.bin",
-    "l10_c1_out.bin",
+    #"l10_c1_out.bin",
     "l10_c2_out.bin",
-    "l11_c1_out.bin",
+    #"l11_c1_out.bin",
     "l11_c2_out.bin",
     "l2_ds_out.bin",
-    "l20_c1_out.bin",
+    #"l20_c1_out.bin",
     "l20_c2_out.bin",
-    "l21_c1_out.bin",
+    #"l21_c1_out.bin",
     "l21_c2_out.bin",
     "l3_ds_out.bin",
-    "l30_c1_out.bin",
+    #"l30_c1_out.bin",
     "l30_c2_out.bin",
-    "l31_c1_out.bin",
+    #"l31_c1_out.bin",
     "l31_c2_out.bin",
     "l4_ds_out.bin",
-    "l40_c1_out.bin",
+    #"l40_c1_out.bin",
     "l40_c2_out.bin",
-    "l41_c1_out.bin",
+    #"l41_c1_out.bin",
     "l41_c2_out.bin",
     "avgpool_out.bin",
     "output.bin" 
 ]
 
+PASS = True
 for file_name  in file_list:
     cpp_output = np.fromfile(os.path.join(cpp_out_path, file_name), dtype=np.float32)
     py_output  = np.fromfile(os.path.join(py_out_path,  file_name), dtype=np.float32)
@@ -47,6 +48,9 @@ for file_name  in file_list:
     print(cpp_output[:10])
     print()
 
+    if mse > 1e-10:
+        PASS = False
+
     if file_name == "output.bin":
         print(f"Expected classificiation: {np.argmax(py_output)}")
         print(f"Actual classificiation: {np.argmax(cpp_output)}")
@@ -55,3 +59,5 @@ for file_name  in file_list:
         plt.plot(np.abs((py_output - cpp_output) / (py_output + 1e-30)))
         plt.show()
         '''
+
+print("PASS" if PASS else "FAIL")
